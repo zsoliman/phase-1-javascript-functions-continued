e74bce200606f8b60efb8a8c819e9d98dd451c6d
Zachary Soliman <zachsoliman1@gmail.com>
2022-07-05 11:02:06 -0400

    commit tree abb778c79197495942a1b06455039ad4b4248315

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+
+const saturdayFun = (activity) => {
+
+    if (!activity) {
+        activity = 'roller-skate'
+    } return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = (work) => {
+    if (!work) {
+        work = 'go to the office'
+    } return `This Monday, I will ${work}.`
+}
+
+const wrapAdjective = (emphatic) => {
+    // let result = wrapAdjective()
+    return ((result) => {
+        if (!result) {
+            result = 'special'
+        }
+        return `You are ${emphatic}${result}${emphatic}!`
+
+    })
+}
